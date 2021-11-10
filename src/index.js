dca958db2ab35c378bdaa486f34d185fcd4da90a
ytur <ytur@softserveinc.com>
2021-11-10 13:23:15 +0200

    Connect redux store to react via react-redux provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux"; // provide redux store data to react components
 
+const store = configureStore(); // we can pass initial state here e.g. from server or local storage
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
